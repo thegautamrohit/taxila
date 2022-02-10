70cd066db50c521a8505dcf34290781309f7bc3f
thegautamrohit <rgautam1997@gmail.com>
2022-02-10 21:31:24 +0530

    why taxila cmpnt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import WhyTaxila from "./Components/WhyTaxila/WhyTaxila";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <WhyTaxila />
+    </div>
+  );
 }
 
 export default App;
