ba51d72d07a035ba54ad88f9d42d440651cada6d
thegautamrohit <rgautam1997@gmail.com>
2022-02-12 14:58:00 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import ContactForm from "./Components/CommonComponents/ContactForm/ContactForm";
+import Header from "./Components/CommonComponents/Header/Header";
 import KeyPoints from "./Components/KeyPoints/KeyPoints";
 import WhyTaxila from "./Components/WhyTaxila/WhyTaxila";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <WhyTaxila />
       <KeyPoints />
       <ContactForm />
--- src/Components/CommonComponents/Header/Header.js
+++ src/Components/CommonComponents/Header/Header.js
@@ -1,8 +1,27 @@
 import React from "react";
+import Logo from "../../../Assets/images/HomePage/Taxila-logo.png";
 import "./Header.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <nav className="navbar-wrapper">
+      <div className="navbar-logo-wrapper">
+        <img src={Logo} alt="" />
+      </div>
+
+      <div className="navbar-quick-links-wrapper">
+        <div className="mono-quick-link">Home</div>
+        <div className="mono-quick-link">Natural</div>
+        <div className="mono-quick-link">Engineered</div>
+        <div className="mono-quick-link">Tiles</div>
+        <div className="mono-quick-link">Accessories</div>
+        <div className="mono-quick-link">Inspiration</div>
+        <div className="mono-quick-link">Resources</div>
+        <div className="mono-quick-link">Shop</div>
+        <div className="mono-quick-link">Media</div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
