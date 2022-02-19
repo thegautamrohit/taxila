759e563fe1bc403f9282e9623c52be8f1086c2a3
thegautamrohit <rgautam1997@gmail.com>
2022-02-19 19:35:27 +0530

    p

--- src/Components/CommonComponents/Header/Header.js
+++ src/Components/CommonComponents/Header/Header.js
@@ -12,15 +12,17 @@ function Header() {
         </div>
 
         <div className="navbar-quick-links-wrapper">
-          <div className="mono-quick-link">Home</div>
+          <div className="mono-quick-link">About</div>
           <div className="mono-quick-link">Natural</div>
           <div className="mono-quick-link">Engineered</div>
           <div className="mono-quick-link">Tiles</div>
+          <div className="mono-quick-link">Kitchen</div>
           <div className="mono-quick-link">Accessories</div>
           <div className="mono-quick-link">Inspiration</div>
           <div className="mono-quick-link">Resources</div>
           <div className="mono-quick-link">Shop</div>
           <div className="mono-quick-link">Media</div>
+          <div className="mono-quick-link">Contact</div>
         </div>
       </nav>
 
--- src/Components/HomePage/MarbleSection/MarbleSection.js
+++ src/Components/HomePage/MarbleSection/MarbleSection.js
@@ -4,101 +4,207 @@ import Marble2 from "../../../Assets/images/HomePage/Marble-2.webp";
 import Marble3 from "../../../Assets/images/HomePage/Marble-3.webp";
 import Marble4 from "../../../Assets/images/HomePage/Marble-4.webp";
 import Marble5 from "../../../Assets/images/HomePage/Marble-5.webp";
+import Slider from "react-slick";
 
 import "./MarbleSection.css";
 
 function MarbleSection() {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+  };
+
   return (
-    <div className="Marble-section-container">
-      {/* tile-1 */}
-      <div className="marble-section-tile-1">
-        <div className="outer-block">
-          <div className="background-outer">
-            <img src={Marble1} alt="" />
-          </div>
-          <span>MARBLE</span>
-        </div>
-
-        <div className="overlay-section">
-          <div className="overlay-content">
+    <>
+      <div className="Marble-section-container-desk">
+        {/* tile-1 */}
+        <div className="marble-section-tile-1">
+          <div className="outer-block">
+            <div className="background-outer">
+              <img src={Marble1} alt="" />
+            </div>
             <span>MARBLE</span>
-            <div className="view-collection-link">View Collection</div>
+          </div>
+
+          <div className="overlay-section">
+            <div className="overlay-content">
+              <span>MARBLE</span>
+              <div className="view-collection-link">View Collection</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Tile-2 */}
+        {/* Tile-2 */}
 
-      <div className="marble-section-tile-2">
-        <div className="outer-block">
-          <div className="background-outer">
-            <img src={Marble2} alt="" />
-          </div>
-          <span>IDYLIUM</span>
-        </div>
-
-        <div className="overlay-section">
-          <div className="overlay-content">
+        <div className="marble-section-tile-2">
+          <div className="outer-block">
+            <div className="background-outer">
+              <img src={Marble2} alt="" />
+            </div>
             <span>IDYLIUM</span>
-            <div className="view-collection-link">View Collection</div>
+          </div>
+
+          <div className="overlay-section">
+            <div className="overlay-content">
+              <span>IDYLIUM</span>
+              <div className="view-collection-link">View Collection</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Tile-3 */}
+        {/* Tile-3 */}
 
-      <div className="marble-section-tile-3">
-        <div className="outer-block">
-          <div className="background-outer">
-            <img src={Marble3} alt="" />
-          </div>
-          <span>QUARTZFORMS</span>
-        </div>
-
-        <div className="overlay-section">
-          <div className="overlay-content">
+        <div className="marble-section-tile-3">
+          <div className="outer-block">
+            <div className="background-outer">
+              <img src={Marble3} alt="" />
+            </div>
             <span>QUARTZFORMS</span>
-            <div className="view-collection-link">View Collection</div>
+          </div>
+
+          <div className="overlay-section">
+            <div className="overlay-content">
+              <span>QUARTZFORMS</span>
+              <div className="view-collection-link">View Collection</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Tile-4 */}
+        {/* Tile-4 */}
 
-      <div className="marble-section-tile-4">
-        <div className="outer-block">
-          <div className="background-outer">
-            <img src={Marble4} alt="" />
-          </div>
-          <span>LE PRESENT</span>
-        </div>
-
-        <div className="overlay-section">
-          <div className="overlay-content">
+        <div className="marble-section-tile-4">
+          <div className="outer-block">
+            <div className="background-outer">
+              <img src={Marble4} alt="" />
+            </div>
             <span>LE PRESENT</span>
-            <div className="view-collection-link">View Collection</div>
+          </div>
+
+          <div className="overlay-section">
+            <div className="overlay-content">
+              <span>LE PRESENT</span>
+              <div className="view-collection-link">View Collection</div>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* Tile-5 */}
+        {/* Tile-5 */}
 
-      <div className="marble-section-tile-5">
-        <div className="outer-block">
-          <div className="background-outer">
-            <img src={Marble5} alt="" />
-          </div>
-          <span>QUARTZITE</span>
-        </div>
-
-        <div className="overlay-section">
-          <div className="overlay-content">
+        <div className="marble-section-tile-5">
+          <div className="outer-block">
+            <div className="background-outer">
+              <img src={Marble5} alt="" />
+            </div>
             <span>QUARTZITE</span>
-            <div className="view-collection-link">View Collection</div>
+          </div>
+
+          <div className="overlay-section">
+            <div className="overlay-content">
+              <span>QUARTZITE</span>
+              <div className="view-collection-link">View Collection</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="marble-section-container-mobile">
+        <div className="marble-section-slider-container">
+          <Slider {...settings}>
+            <div className="marble-section-tile-1">
+              <div className="outer-block">
+                <div className="background-outer">
+                  <img src={Marble1} alt="" />
+                </div>
+                <span>MARBLE</span>
+              </div>
+
+              <div className="overlay-section">
+                <div className="overlay-content">
+                  <span>MARBLE</span>
+                  <div className="view-collection-link">View Collection</div>
+                </div>
+              </div>
+            </div>
+
+            {/* Tile-2 */}
+
+            <div className="marble-section-tile-2">
+              <div className="outer-block">
+                <div className="background-outer">
+                  <img src={Marble2} alt="" />
+                </div>
+                <span>IDYLIUM</span>
+              </div>
+
+              <div className="overlay-section">
+                <div className="overlay-content">
+                  <span>IDYLIUM</span>
+                  <div className="view-collection-link">View Collection</div>
+                </div>
+              </div>
+            </div>
+
+            {/* Tile-3 */}
+
+            <div className="marble-section-tile-3">
+              <div className="outer-block">
+                <div className="background-outer">
+                  <img src={Marble3} alt="" />
+                </div>
+                <span>QUARTZFORMS</span>
+              </div>
+
+              <div className="overlay-section">
+                <div className="overlay-content">
+                  <span>QUARTZFORMS</span>
+                  <div className="view-collection-link">View Collection</div>
+                </div>
+              </div>
+            </div>
+
+            {/* Tile-4 */}
+
+            <div className="marble-section-tile-4">
+              <div className="outer-block">
+                <div className="background-outer">
+                  <img src={Marble4} alt="" />
+                </div>
+                <span>LE PRESENT</span>
+              </div>
+
+              <div className="overlay-section">
+                <div className="overlay-content">
+                  <span>LE PRESENT</span>
+                  <div className="view-collection-link">View Collection</div>
+                </div>
+              </div>
+            </div>
+
+            {/* Tile-5 */}
+
+            <div className="marble-section-tile-5">
+              <div className="outer-block">
+                <div className="background-outer">
+                  <img src={Marble5} alt="" />
+                </div>
+                <span>QUARTZITE</span>
+              </div>
+
+              <div className="overlay-section">
+                <div className="overlay-content">
+                  <span>QUARTZITE</span>
+                  <div className="view-collection-link">View Collection</div>
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </>
   );
 }
 
