db55d0fe2a09d66ee6173b72aa02b34b9f18a7c8
thegautamrohit <rgautam1997@gmail.com>
2022-03-25 20:59:18 +0530

    home page chnages

--- src/Components/HomePage/WhyTaxila/WhyTaxila.js
+++ src/Components/HomePage/WhyTaxila/WhyTaxila.js
@@ -3,59 +3,64 @@ import "./WhyTaxila.css";
 import About1 from "../../../Assets/images/HomePage/About-us-1.webp";
 import About2 from "../../../Assets/images/HomePage/About-us-2.webp";
 import { IoIosArrowForward } from "react-icons/io";
+import Fade from "react-reveal/Fade";
 
 function WhyTaxila() {
   return (
     <div className="why-taxila-container">
-      <div className="why-taxila-upper-section">
-        <div className="why-taxila-grid-1">
-          <div className="why-taxila-img-container">
-            <img src={About1} alt="About us" />
-          </div>
-        </div>
-
-        <div className="why-taxila-grid-2">
-          <h4 className="why-taxila-grid-head">
-            WHY
-            <br />
-            <span>TAXILA STONE</span>
-          </h4>
-
-          <p className="why-taxila-grid-subhead-1">
-            A one-stop solution for designers and builders alike, we at Taxila
-            Stone strive to bring the best quality local and imported stone
-            surfaces from all over the globe. Without the involvement of any
-            third-party vendors, Taxila Stone offers unparalleled and reliable
-            customer service to build the home of your dreams.
-          </p>
-        </div>
-      </div>
-
-      <div className="why-taxila-lower-section">
-        <div className="why-taxila-grid-3">
-          <p className="why-taxila-grid-subhead-2">
-            We build stones that define you - Taxila Stone seeks inspiration
-            after the lost city of cut stone -Takshashila. Our mission is to
-            share the hidden beauty behind the legacy, the art, the colors, and
-            the people that inspire us to curate the perfect stone surfaces for
-            you.
-          </p>
-
-          <button className="why-taxila-btn">
-            <span>About Us</span>
-            <div className="why-taxila-btn-hover">
-              <div className="why-taxila-btn-circle"> </div>
-              <IoIosArrowForward />
+      <Fade bottom>
+        <div className="why-taxila-upper-section">
+          <div className="why-taxila-grid-1">
+            <div className="why-taxila-img-container">
+              <img src={About1} alt="About us" />
             </div>
-          </button>
-        </div>
+          </div>
 
-        <div className="why-taxila-grid-4">
-          <div className="why-taxila-img-container">
-            <img src={About2} alt="About us" />
+          <div className="why-taxila-grid-2">
+            <h4 className="why-taxila-grid-head">
+              WHY
+              <br />
+              <span>TAXILA STONE</span>
+            </h4>
+
+            <p className="why-taxila-grid-subhead-1">
+              A one-stop solution for designers and builders alike, we at Taxila
+              Stone strive to bring the best quality local and imported stone
+              surfaces from all over the globe. Without the involvement of any
+              third-party vendors, Taxila Stone offers unparalleled and reliable
+              customer service to build the home of your dreams.
+            </p>
           </div>
         </div>
-      </div>
+      </Fade>
+
+      <Fade bottom>
+        <div className="why-taxila-lower-section">
+          <div className="why-taxila-grid-3">
+            <p className="why-taxila-grid-subhead-2">
+              We build stones that define you - Taxila Stone seeks inspiration
+              after the lost city of cut stone -Takshashila. Our mission is to
+              share the hidden beauty behind the legacy, the art, the colors,
+              and the people that inspire us to curate the perfect stone
+              surfaces for you.
+            </p>
+
+            <button className="why-taxila-btn">
+              <span>About Us</span>
+              <div className="why-taxila-btn-hover">
+                <div className="why-taxila-btn-circle"> </div>
+                <IoIosArrowForward />
+              </div>
+            </button>
+          </div>
+
+          <div className="why-taxila-grid-4">
+            <div className="why-taxila-img-container">
+              <img src={About2} alt="About us" />
+            </div>
+          </div>
+        </div>
+      </Fade>
     </div>
   );
 }
