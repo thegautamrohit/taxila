56f56addf51d1825d90f01f479573c185a20a7d1
thegautamrohit <rgautam1997@gmail.com>
2022-03-29 22:49:57 +0530

    Media page data added

--- src/Components/Media/MediaContainer/MediaContainer.js
+++ src/Components/Media/MediaContainer/MediaContainer.js
@@ -1,63 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Image from "../../../Assets/images/HomePage/Banner-2.webp";
 import "./MediaContainer.css";
 import { IoIosArrowForward } from "react-icons/io";
+import { mediaLinks } from "../../../Media Links";
 
 function MediaContainer() {
   const [toggle, setToggle] = useState(0);
   const [data, setData] = useState();
 
-  const media = [
-    {
-      id: 1,
-      category: "Feature",
-      image: "",
-      head: "Featured on Paper City Magazine",
-      date: "22 March, 2020",
-      summary: "",
-      link: "http://papercitymagazine.uberflip.com/i/1439511-papercity-dallas-january-february-2022/15?",
-    },
-    {
-      id: 2,
-      category: "Feature",
-      image: "",
-      head: "Featured on Dallas Modern Luxury",
-      date: "22 March, 2020",
-      summary: "",
-      link: "https://digital.modernluxury.com/publication/?m=3620&i=738656&p=56&ver=html5",
-    },
-    {
-      id: 3,
-      category: "Article",
-      image: "",
-      head: "Conversations with Josh Caballero",
-      date: "17 March, 2022",
-      summary:
-        "Hi Josh, so excited to have you with us today. What can you tell us about your story? How we got here is a complex story. Here’s the short version – The principals of the three show rooms opening in the metroplex come from hospitality and furniture backgrounds. We are creating the showrooms we feel would have made some of our projects in the past easier. We want to offer a wide variety of hard-to-find and fashionable materials and furnishings to DFW from all over the world. ",
-      link: "http://voyagedallas.com/interview/conversations-with-josh-caballero/",
-    },
-    {
-      id: 4,
-      category: "Feature",
-      image: "",
-      head: "Featured on Paper City Magazine",
-      date: "17 March, 2022",
-      summary: "",
-      link: "http://papercitymagazine.uberflip.com/i/1455228-papercity-dallas-march-2022/57?",
-    },
-    {
-      id: 5,
-      category: "Feature",
-      image: "",
-      head: "Featured on Dallas Modern Luxury",
-      date: "17 March, 2022",
-      summary: "",
-      link: "http://digital.modernluxury.com/publication/?m=3620&i=729885&p=186&ver=html5",
-    },
-  ];
-
   useEffect(() => {
-    let arr = [media.filter((m) => m.category === "Article")];
+    let arr = [mediaLinks.filter((m) => m.CATEGORY === "Article")];
 
     setData(arr);
   }, []);
@@ -65,18 +16,14 @@ function MediaContainer() {
   const clickHandler = (toggle) => {
     setToggle(toggle);
     if (toggle === 0) {
-      let arr = [media.filter((m) => m.category === "Article")];
-
+      let arr = [mediaLinks.filter((m) => m.CATEGORY === "Article")];
       setData(arr);
     }
     if (toggle === 1) {
-      let arr = [media.filter((m) => m.category === "Feature")];
-
+      let arr = [mediaLinks.filter((m) => m.CATEGORY === "Featured")];
       setData(arr);
     }
   };
-
-  console.log(data);
 
   return (
     <div className="media-container-wrapper">
@@ -106,18 +53,18 @@ function MediaContainer() {
           data[0]?.map((d) => (
             <div key={d.id} className="media-container-mono-article">
               <div className="media-article-img-box">
-                <img src={Image} alt="media article" />
+                <img src={d?.IMAGE} alt="media article" />
               </div>
 
               <div className="media-article-content">
-                <h5 className="media-article-title">{d?.head}</h5>
+                <h5 className="media-article-title">{d?.HEADING}</h5>
 
-                <span className="media-article-date"> {d?.date} </span>
+                <span className="media-article-date"> {d?.DATE} </span>
                 {d.summary && (
-                  <p className="media-article-summary">{d?.summary}</p>
+                  <p className="media-article-summary">{d?.BODY_TEXT}</p>
                 )}
 
-                <a href={d?.link} target="_blank" className="media-article-btn">
+                <a href={d?.LINK} target="_blank" className="media-article-btn">
                   <span>Read More</span>
                   <div className="media-article-btn-hover">
                     <div className="media-article-btn-circle"> </div>
---
+++ src/Media Links.js
@@ -0,0 +1,163 @@
+import Image1 from "./Assets/images/Media/1. CI Newspaper - July 3-Aug 7, 2021 (convert.io).webp";
+import Image2 from "./Assets/images/Media/2. Paper City - Dallas, September 2021 (convert.io).webp";
+import Image3 from "./Assets/images/Media/3. CI Newspaper - Sept 6 -Oct 2, 2021.webp";
+import Image4 from "./Assets/images/Media/4. CI Newspaper - Oct 3 - 31 2021.webp";
+import Image5 from "./Assets/images/Media/5. Paper City - Dallas, November 2021.webp";
+import Image6 from "./Assets/images/Media/6. CI Newspaper - Nov 1 - Dec 5 2021.webp";
+import Image7 from "./Assets/images/Media/7. Paper City Article - December 9, 2021.webp";
+import Image8 from "./Assets/images/Media/8. Modern Luxury - Dallas, December 2021.webp";
+import Image9 from "./Assets/images/Media/9. Paper City - Dallas, January _ February 2022.webp";
+import Image10 from "./Assets/images/Media/10. Modern Luxury Interiors - Texas, February 2022.webp";
+import Image11 from "./Assets/images/Media/11. Modern Luxury - Dallas, March 2022.webp";
+import Image12 from "./Assets/images/Media/12. Paper City - Dallas, March 2022.webp";
+import Image13 from "./Assets/images/Media/13. Voyage Dallas - March 17, 2022.webp";
+
+export const mediaLinks = [
+  {
+    id: 1,
+    IMAGE: Image1,
+    CATEGORY: "Featured",
+    WEBSITE: "Community Impact Newspaper",
+    DATE: " July 3 - Aug 7, 2021",
+    EDITION: "Volume 11, Issue 5, Page 16",
+    LINK: "https://editions.communityimpact.com/view/704356651/16/",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 2,
+    IMAGE: Image2,
+    CATEGORY: "Featured",
+    WEBSITE: "Paper City",
+    DATE: "September 2021",
+    EDITION: "Dallas, Page 13 - 14",
+    LINK: "http://papercitymagazine.uberflip.com/i/1405904-papercity-dallas-september-2021/13?",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 3,
+    IMAGE: Image3,
+    CATEGORY: "Featured",
+    WEBSITE: "Community Impact Newspaper",
+    DATE: "Sept 6 - Oct 2, 2021",
+    EDITION: "Volume 11, Issue 7, Page 22",
+    LINK: "https://editions.communityimpact.com/view/838292375/22/",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 4,
+    IMAGE: Image4,
+    CATEGORY: "Featured",
+    WEBSITE: "Community Impact Newspaper",
+    DATE: "Oct 3 - 31, 2021",
+    EDITION: "Volume 11, Issue 8, Page 10",
+    LINK: "https://editions.communityimpact.com/view/533419637/10/",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 5,
+    IMAGE: Image5,
+    CATEGORY: "Featured",
+    WEBSITE: "Paper City",
+    DATE: "November 2021",
+    EDITION: "Dallas, Page 15 - 16",
+    LINK: "http://papercitymagazine.uberflip.com/i/1420844-papercity-dallas-november-2021/15?",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 6,
+    IMAGE: Image6,
+    CATEGORY: "Featured",
+    WEBSITE: "Community Impact Newspaper",
+    DATE: "Nov 1 - Dec 5, 2021",
+    EDITION: "Volume 11, Issue 9, Page 14",
+    LINK: "https://editions.communityimpact.com/view/770494646/14/",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 7,
+    IMAGE: Image7,
+    CATEGORY: "Article",
+    WEBSITE: "Paper City",
+    DATE: "December 9, 2021",
+    EDITION: null,
+    LINK: "https://www.papercitymag.com/society/dallas-design-taxila-stone-showroom-surfaces-stone-artists-designers/",
+    HEADING:
+      "Taxila Stone Celebrates Local Artists and Designers at Its Dallas Showroom",
+    BODY_TEXT:
+      "An inspiring night for design professionals, the Taxila Stone Designer Showroom event was well attended by a variety of industry leaders — from local property owners and architects to the city’s most celebrated designers, talented artists, and design-savvy individuals in the community.",
+  },
+  {
+    id: 8,
+    IMAGE: Image8,
+    CATEGORY: "Article",
+    WEBSITE: "Modern Luxury",
+    DATE: "December 2021",
+    EDITION: "Dallas, Page 184",
+    LINK: "http://digital.modernluxury.com/publication/?m=3620&i=729885&p=186&ver=html5",
+    HEADING: "Interview with the CEO, Hari Polavarapu",
+    BODY_TEXT:
+      "As the CEO of Taxila Stone, a wholesale distributor of hand-selected natural and engineered stone surfaces, Hari Polavarapu is surrounded with timeless beauty.",
+  },
+  {
+    id: 9,
+    IMAGE: Image9,
+    CATEGORY: "Featured",
+    WEBSITE: "Paper City",
+    DATE: "January & February 2022",
+    EDITION: "Dallas, Page 15 - 16",
+    LINK: "http://papercitymagazine.uberflip.com/i/1439511-papercity-dallas-january-february-2022/15?",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 10,
+    IMAGE: Image10,
+    CATEGORY: "Featured",
+    WEBSITE: "Modern Luxury - Interiors",
+    DATE: "Febraury 2022",
+    EDITION: "Texas, Page 22 - 23",
+    LINK: "https://digital.modernluxury.com/publication/?m=22590&i=737416&p=24&ver=html5",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 11,
+    IMAGE: Image11,
+    CATEGORY: "Featured",
+    WEBSITE: "Modern Luxury",
+    DATE: "March 2022",
+    EDITION: "Dallas, Page 55",
+    LINK: "https://digital.modernluxury.com/publication/?m=3620&i=738656&p=56&ver=html5",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 12,
+    IMAGE: Image12,
+    CATEGORY: "Featured",
+    WEBSITE: "Paper City",
+    DATE: "March 2022",
+    EDITION: "Dallas, Page 58",
+    LINK: "http://papercitymagazine.uberflip.com/i/1455228-papercity-dallas-march-2022/57?",
+    HEADING: null,
+    BODY_TEXT: null,
+  },
+  {
+    id: 13,
+    IMAGE: Image13,
+    CATEGORY: "Article",
+    WEBSITE: "Voyage Dallas",
+    DATE: "March 17, 2022",
+    EDITION: null,
+    LINK: "http://voyagedallas.com/interview/conversations-with-josh-caballero/",
+    HEADING: "Conversations with Josh Caballero",
+    BODY_TEXT:
+      "Hi Josh, so excited to have you with us today. What can you tell us about your story?\nHow we got here is a complex story. Here’s the short version – The principals of the three show rooms opening in the metroplex come from hospitality and furniture backgrounds.",
+  },
+];
