e431dfc2efbe79696a061174990ef83739fd9b8f
Ajay Upadhyay <ajay.bhumca18@gmail.com>
2022-04-07 21:30:30 +0530

    mobile view

--- src/Components/CommonComponents/carousal/carousalKitchen.js
+++ src/Components/CommonComponents/carousal/carousalKitchen.js
@@ -28,60 +28,119 @@ const Carousel = ({ children, details }) => {
   };
 
   useEffect(() => {
-    if (window.innerWidth < "450px") {
+    // if (window.innerWidth < "450px") {
+    //   setisMobile(true);
+    // }
+    // const interval = setInterval(() => {
+    //   if (!paused) {
+    //     updateIndex(activeIndex + 1);
+    //   }
+    if (window.innerWidth < 500) {
       setisMobile(true);
     }
-    const interval = setInterval(() => {
-      if (!paused) {
-        updateIndex(activeIndex + 1);
-      }
-    }, 3000);
-
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  });
+  }, [window.innerWidth]);
 
   const handlers = useSwipeable({
     onSwipedLeft: () => updateIndex(activeIndex + 1),
     onSwipedRight: () => updateIndex(activeIndex - 1),
   });
 
+  console.log(window.innerWidth);
   return (
-    <div className="kitchen__carousal__container">
-      {showDetails ? (
-        <div className="kitchen__carousal__data">
-          <p className="kitchen__carousal__data__heading">Introduction</p>
-          <p className="kitchen__carousal__data__details">{details}</p>
-          <p
-            className="kitchen__carousal__data__show"
-            onClick={() => setShowDetails(!showDetails)}
-          >
-            Explore Images
-          </p>
-        </div>
-      ) : (
-        <div
-          {...handlers}
-          className="carousel"
-          onMouseEnter={() => setPaused(true)}
-          onMouseLeave={() => setPaused(false)}
-        >
-          <div
-            className="inner"
-            style={{
-              transform: `translateX(-${952 * activeIndex + 0.5}px)`,
-            }}
-          >
-            {React.Children.map(children, (child, index) => {
-              return React.cloneElement(child, { width: "911px" });
-            })}
+    <>
+      <div className="kitchen__carousal__container__desktop">
+        {showDetails ? (
+          <div className="kitchen__carousal__data">
+            <p className="kitchen__carousal__data__heading">Introduction</p>
+            <p className="kitchen__carousal__data__details">{details}</p>
+            <p
+              className="kitchen__carousal__data__show"
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              Explore Images
+            </p>
           </div>
-          <div className="kitchen__carousal__arrow">
-            <p onClick={() => setShowDetails(!showDetails)}>Explore Details</p>
-            <div>
+        ) : (
+          <div
+            {...handlers}
+            className="carousel"
+            onMouseEnter={() => setPaused(true)}
+            onMouseLeave={() => setPaused(false)}
+          >
+            <div
+              className="inner"
+              style={{
+                transform: `translateX(-${952 * activeIndex + 0.5}px)`,
+              }}
+            >
+              {React.Children.map(children, (child, index) => {
+                return React.cloneElement(child, { width: "911px" });
+              })}
+            </div>
+            <div className="kitchen__carousal__arrow">
+              <p onClick={() => setShowDetails(!showDetails)}>
+                Explore Details
+              </p>
+              <div>
+                <BsArrowLeft
+                  onClick={() => {
+                    updateIndex(activeIndex - 1);
+                  }}
+                />
+                <BsArrowRight
+                  onClick={() => {
+                    updateIndex(activeIndex + 1);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="kitchen__carousal__details">
+              <p>0{activeIndex + 1}</p> <p>-</p>{" "}
+              <p>0{React.Children.count(children)}</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="kitchen__carousal__container__mobile">
+        {showDetails ? (
+          <div className="kitchen__carousal__data">
+            <p className="kitchen__carousal__data__heading">Introduction</p>
+            <p className="kitchen__carousal__data__details">{details}</p>
+            <p
+              className="kitchen__carousal__data__show"
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              Explore Images
+            </p>
+          </div>
+        ) : (
+          <div
+            {...handlers}
+            className="carousel"
+            onMouseEnter={() => setPaused(true)}
+            onMouseLeave={() => setPaused(false)}
+          >
+            <div
+              className="inner"
+              style={{
+                transform: `translateX(-${355 * activeIndex + 0.5}px)`,
+              }}
+            >
+              {React.Children.map(children, (child, index) => {
+                return React.cloneElement(child, { width: "911px" });
+              })}
+            </div>
+            <div className="kitchen__carousal__arrow">
+              <p onClick={() => setShowDetails(!showDetails)}>
+                Explore Details
+              </p>
+              <div className="kitchen__carousal__details">
+                <p>0{activeIndex + 1}</p> <p>-</p>{" "}
+                <p>0{React.Children.count(children)}</p>
+              </div>
+            </div>
+
+            <div className="kitchen__carousal__arrow__mobile">
               <BsArrowLeft
                 onClick={() => {
                   updateIndex(activeIndex - 1);
@@ -94,13 +153,9 @@ const Carousel = ({ children, details }) => {
               />
             </div>
           </div>
-          <div className="kitchen__carousal__details">
-            <p>0{activeIndex + 1}</p> <p>-</p>{" "}
-            <p>0{React.Children.count(children)}</p>
-          </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
 
