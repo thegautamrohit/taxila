c3f2450c5b098a6d1e9dfd49356e753b135b92a4
thegautamrohit <rgautam1997@gmail.com>
2022-05-08 18:21:55 +0530

    about page

--- src/Components/Contact/ContactSection/ContactSection.js
+++ src/Components/Contact/ContactSection/ContactSection.js
@@ -81,7 +81,7 @@ function ContactSection() {
 
           <div className="contact__section__map__wrapper">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.975846955995!2d-96.8201453844317!3d32.79289969048182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9948bcb5c615%3A0xd3320a9fb870014d!2s1436%20Slocum%20St%2C%20Dallas%2C%20TX%2075207%2C%20USA!5e0!3m2!1sen!2sin!4v1651912770904!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.498132880643!2d-96.87146508443145!3d32.80556398983558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9c798ff7e6ad%3A0xe9d6d6396c7556ea!2s4843%20Calvert%20St%2C%20Dallas%2C%20TX%2075247%2C%20USA!5e0!3m2!1sen!2sin!4v1651915800460!5m2!1sen!2sin"
               //   width="400"
               //   height="300"
               //   style="border:0;"
@@ -125,7 +125,7 @@ function ContactSection() {
 
           <div className="contact__section__map__wrapper">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.498132880643!2d-96.87146508443145!3d32.80556398983558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9c798ff7e6ad%3A0xe9d6d6396c7556ea!2s4843%20Calvert%20St%2C%20Dallas%2C%20TX%2075247%2C%20USA!5e0!3m2!1sen!2sin!4v1651915800460!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3353.975846955995!2d-96.8201453844317!3d32.79289969048182!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e9948bcb5c615%3A0xd3320a9fb870014d!2s1436%20Slocum%20St%2C%20Dallas%2C%20TX%2075207%2C%20USA!5e0!3m2!1sen!2sin!4v1651912770904!5m2!1sen!2sin"
               //   width="400"
               //   height="300"
               //   style="border:0;"
